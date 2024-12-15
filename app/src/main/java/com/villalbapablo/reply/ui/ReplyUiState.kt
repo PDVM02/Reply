d01d8a4636150c063c44f336cbab0532d96d0b99
PDVM02 <pablo.villalba2002@gmail.com>
2024-12-15 00:40:08 -0300

    Initial commit

---
+++ app/src/main/java/com/villalbapablo/reply/ui/ReplyUiState.kt
@@ -0,0 +1,14 @@
+package com.villalbapablo.reply.ui
+
+import com.villalbapablo.reply.data.Email
+import com.villalbapablo.reply.data.MailboxType
+import com.villalbapablo.reply.data.local.LocalEmailsDataProvider
+
+data class ReplyUiState(
+    val mailboxes: Map<MailboxType, List<Email>> = emptyMap(),
+    val currentMailbox: MailboxType = MailboxType.Inbox,
+    val currentSelectedEmail: Email = LocalEmailsDataProvider.defaultEmail,
+    val isShowingHomepage: Boolean = true
+) {
+    val currentMailboxEmails: List<Email> by lazy { mailboxes[currentMailbox]!! }
+}
